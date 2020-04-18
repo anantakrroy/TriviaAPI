b42766117533ac4b63343bdd0682a73859eaa85a
Ananta Kr. Roy <kr.innovate@gmail.com>
2020-04-18 15:55:39 +0530

    Added unit tests for questions and categories routes

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -13,9 +13,9 @@ class TriviaTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app()
-        self.client = self.app.test_client
+        self.client = self.app.test_client()
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://postgres@{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +33,26 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    # Test categories
+    def test_get_categories(self):
+        res = self.client.get('/categories')
+        data = json.loads(res.data)
+       
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(data['categories']),6)
 
+    # Test questions
+    def test_get_questions(self):
+        res = self.client.get('/questions?page=1')
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['categories'])
+        self.assertTrue(data['questions'])
+        self.assertEqual(data['total_questions'], 19)
+
+    # Test delete question
+    
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
